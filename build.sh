1ba993df95689019ef4c05864e7816e38a8faf59
Niels Martignène <niels.martignene@protonmail.com>
2022-02-12 11:21:19 +0100

    Support basic options in build.sh

--- build.sh
+++ build.sh
@@ -2,8 +2,28 @@
 
 cd $(dirname $0)
 
-vendor/node/configure --without-intl
-make -C vendor/node $*
+print_usage() {
+    echo "Usage: ./build.sh [--reconfigure] [-j jobs]"
+}
+
+configure=1
+cores=1
+
+test -f vendor/node/config.status && configure=0
+
+while [ $# -gt 0 ]; do
+    case "$1" in
+        -r|--reconfigure) configure=1 ;;
+        -j|--cores) cores=$2; shift ;;
+        --help) print_usage; exit 0 ;;
+        *) echo "Unknown option $1"; exit 1 ;;
+    esac
+
+    shift
+done
+
+if [ "$configure" = 1 ]; then vendor/node/configure --without-intl; fi
+make -C vendor/node -j $cores
 install -m 0755 vendor/node/out/Release/node ./luigi
 
 vendor/esbuild/esbuild_linux_x64 --bundle src/luigi/luigi.js --outfile=luigi.js --platform=node --minify
